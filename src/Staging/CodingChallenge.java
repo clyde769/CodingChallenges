b66fcd0fb4666cd742b684135bb5c07b5146f7e9
Chris Christian <clyde769@revature.net>
2022-07-01 13:19:23 -0500

    Added the new challenges for finding distance and yourNameIsMyName

--- src/Staging/CodingChallenge.java
+++ src/Staging/CodingChallenge.java
@@ -1,10 +1,45 @@
 package Staging;
 
 public class CodingChallenge {
-    public static int findNumberOfSquare(int a, int b) {
-        double init = Math.sqrt(a);
-        double finalValue = Math.sqrt(b);
-        int result = (int) (Math.floor(finalValue) - Math.ceil(init)+1);
+	
+	//1
+    public static int findDistance(int[][] arry, int N) {
+    	int result = 0;
+    	int j = 0;
+    	int k =0;
+        for (int i=N; i>=1; i--) {
+            	result+= arry[i][j]-arry[k][0];
+            	result+= Math.abs(arry[j][0]-arry[i][j]);
+            	k++;
+        }
         return result;
     }
-}
+    
+    //2
+    public static boolean yourNameIsMyName(String value, String testString) {
+    	int index = -1;
+    	//add string to string builder class
+    	 StringBuilder str = new StringBuilder(testString);
+    	 
+    	//loop through each character in string
+    	  for (int i = 0; i < value.length(); i++) {
+    		  //when it fails automatically return false else just continue.  The index must be larger to ensure duplicate characters in string work
+    		  try 
+    		  {
+    	            char letter = value.charAt(i);
+    	            String stringLetter = Character.toString(letter);
+    	            int position = str.indexOf(stringLetter);
+    	            str.deleteCharAt(position);
+    	             if (position < index) {
+    	            	 return false;
+    	             }else {
+    	            	 index = position;
+    	             }
+    		  } catch (Exception e){ 
+    			  return false;
+    		  }
+          }
+    	
+    	return true;
+    }
+} 
--- src/Staging/Driver2.java
+++ src/Staging/Driver2.java
@@ -3,7 +3,10 @@ package Staging;
 public class Driver2 {
 
 	public static void main(String[] args) {
-		  System.out.println(CodingChallenge.findNumberOfSquare(3,9));
+		int[][] test = {{0,0},{1,0},{2,0}};
+		  System.out.println(CodingChallenge.findDistance(test,2));
+		  
+		 System.out.println(CodingChallenge.yourNameIsMyName("john", "johanna"));
 	}
 
 }
